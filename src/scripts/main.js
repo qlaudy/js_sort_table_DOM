3af42f5741cf923e1e57e85e6c9fc8fa756ccc51
unknown <dkuz6323@gmail.com>
2026-01-28 15:46:46 +0200

    fix bug

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -25,6 +25,13 @@ thead.addEventListener('click', (events) => {
     const textA = indexA.textContent;
     const textB = indexB.textContent;
 
+    const numA = Number(textA.replace(/[^0-9.-]/g, ''));
+    const numB = Number(textB.replace(/[^0-9.-]/g, ''));
+
+    if (!Number.isNaN(numA) && !Number.isNaN(numB)) {
+      return numA - numB;
+    }
+
     return textA.localeCompare(textB);
   });
 
