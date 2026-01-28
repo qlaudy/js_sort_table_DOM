75439ef13066f070d3a0609e008cd9f7238cf1a0
unknown <dkuz6323@gmail.com>
2026-01-28 15:40:30 +0200

    completed tasks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
 // write code here
+
+const thead = document.querySelector('thead');
+
+thead.addEventListener('click', (events) => {
+  const th = events.target.closest('th');
+
+  if (!th) {
+    return 0;
+  }
+
+  const i = th.cellIndex;
+
+  const tbody = document.querySelector('tbody');
+  const tr = tbody.querySelectorAll('tr');
+
+  const trArray = Array.from(tr);
+
+  trArray.sort((a, b) => {
+    const indexA = a.children[i];
+    const indexB = b.children[i];
+
+    const textA = indexA.textContent;
+    const textB = indexB.textContent;
+
+    return textA.localeCompare(textB);
+  });
+
+  tbody.innerHTML = '';
+
+  trArray.forEach((el) => {
+    tbody.append(el);
+  });
+});
